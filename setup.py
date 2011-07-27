5b0e932612e146ca620b5aa09a5631a758ceaad0
Douglas Schilling Landgraf <dougsland@gmail.com>
2011-07-27 02:29:15 -0300

    fixed module name

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(name='python-confparser', version='1.0.0',
           'Programming Language :: Python',
           ],
       license= 'LGPLv2+' ,
-      py_modules = ['python-confparser']   
+      py_modules = ['confparser']   
            
 )
