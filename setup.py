d4a9e1d9ecc75ff8796be5efef50193dacd208a3
Douglas Schilling Landgraf <dougsland@gmail.com>
2011-07-27 01:01:59 -0300

    fixed the name of project/url

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
 
-setup(name='confparse', version='1.0.0',
+setup(name='python-confparse', version='1.0.0',
       description='Parser for *nix config files',
       author='Douglas Schilling Landgraf',
       author_email='dougsland@redhat.com',
-      url='https://github.com/dougsland/confparse',
+      url='https://github.com/dougsland/python-confparse',
       classifiers=[
           'Environment :: Console',
           'Intended Audience :: Developers',
           'License :: GPLv2',
           'Programming Language :: Python',
           ],
-      license= 'GPLv2' ,
-      py_modules = ['confparse']   
+      license= 'LGPLv2+' ,
+      py_modules = ['python-confparse']   
            
 )
